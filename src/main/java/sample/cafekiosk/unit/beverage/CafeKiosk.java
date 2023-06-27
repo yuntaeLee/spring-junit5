1e13b7dbcaed5a4620633107402e4a09e3fa29b1
yoon <leeyt1201@gmail.com>
2023-06-28 08:08:35 +0900

    refactor: changed the method that add several beverages to not use for loop

--- src/main/java/sample/cafekiosk/unit/beverage/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/beverage/CafeKiosk.java
@@ -6,6 +6,7 @@ import sample.cafekiosk.unit.order.Order;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Getter
@@ -25,9 +26,7 @@ public class CafeKiosk {
             throw new IllegalArgumentException("음료는 1잔 이상 주문하실 수 있습니다.");
         }
 
-        for (int i = 0; i < count; i++) {
-            beverages.add(beverage);
-        }
+        beverages.addAll(Collections.nCopies(count, beverage));
     }
 
     public void remove(Beverage beverage) {
@@ -44,6 +43,18 @@ public class CafeKiosk {
                 .sum();
     }
 
+    // 테스트 어려움
+    public Order createOrder() {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        LocalTime currentTime = LocalDateTime.now().toLocalTime();
+        if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
+            throw new IllegalArgumentException("주문 시간이 아닙니다. 관리자에게 문의하세요.");
+        }
+
+        return new Order(currentDateTime, beverages);
+    }
+
+    // 테스트 용이
     public Order createOrder(LocalDateTime currentDateTime) {
         LocalTime currentTime = currentDateTime.toLocalTime();
         if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
